ac3cb68960bcb0908b8abd200bae9608b056f09b
tkreyling <thomas.kreyling@googlemail.com>
2017-10-03 18:34:48 +0200

    Downgraded to junit 4 and added gradle plugin "java"
    * downgraded to junit 4 because of a problem with spring context and mbeans in junit 5
      (executing multiple tests with junit 5 caused an UnableToRegisterMBeanException for HikariDataSource (testdb))
    * added gradle plugin "java" because compileOnly seems to be defined through plugin "java" which was pulled in as transitive dependency through junit 5 plugin

--- src/test/java/catalog/ProductControllerTest.java
+++ src/test/java/catalog/ProductControllerTest.java
@@ -1,23 +1,26 @@
 package catalog;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringJUnitWebConfig(Application.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
-class ProductControllerTest {
+public class ProductControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    void getProducts() throws Exception {
+    public void getProducts() throws Exception {
         mvc.perform(get("/products"))
                 .andExpect(status().is2xxSuccessful());
     }
