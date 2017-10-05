eb801ee8518e722c66c0c5fcb7631ca9a89caea2
tkreyling <thomas.kreyling@googlemail.com>
2017-10-05 17:38:28 +0200

    Organized imports

--- src/test/java/catalog/product/ProductControllerTest.java
+++ src/test/java/catalog/product/ProductControllerTest.java
@@ -1,7 +1,6 @@
 package catalog.product;
 
 import catalog.Application;
-import catalog.product.ProductDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
