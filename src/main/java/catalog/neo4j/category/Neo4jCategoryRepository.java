99611626a121c2b8e7b5a3e40e0d7ea704c056dd
tkreyling <thomas.kreyling@googlemail.com>
2017-10-08 23:11:43 +0200

    Unified formatting of Cypher keywords (fully capitalized)

--- src/main/java/catalog/neo4j/category/Neo4jCategoryRepository.java
+++ src/main/java/catalog/neo4j/category/Neo4jCategoryRepository.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public interface Neo4jCategoryRepository extends Neo4jRepository<Category, Long> {
 
-    @Query("MATCH (c:Category)-[:PARENT]->(p:Category) WHERE id(p)={id} return c")
+    @Query("MATCH (c:Category)-[:PARENT]->(p:Category) WHERE id(p)={id} RETURN c")
     List<Category> findByParentId(@Param("id") Long id);
 
-    @Query("MATCH (c:Category)-[:PARENT*]->(p:Category) WHERE id(c)={id} return p")
+    @Query("MATCH (c:Category)-[:PARENT*]->(p:Category) WHERE id(c)={id} RETURN p")
     Category findRootNode(@Param("id") Long id);
 
-    @Query("MATCH path = (c:Category)-[:PARENT*]->(p:Category) WHERE id(c)={id} return path")
+    @Query("MATCH path = (c:Category)-[:PARENT*]->(p:Category) WHERE id(c)={id} RETURN path")
     List<Category> loadCategoryPath(@Param("id") Long id);
 
     List<Category> findByName(String name);
