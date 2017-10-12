99adc63299a0bff1e73911e43e29e750a2a42b8f
tkreyling <thomas.kreyling@googlemail.com>
2017-10-12 22:01:31 +0200

    Organized imports

--- src/test/java/catalog/category/CategoryControllerTest.java
+++ src/test/java/catalog/category/CategoryControllerTest.java
@@ -1,17 +1,14 @@
 package catalog.category;
 
 import catalog.Application;
-import catalog.product.ProductDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-import lombok.Value;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
